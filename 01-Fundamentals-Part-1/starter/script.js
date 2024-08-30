17948ba21f3bde8ea33cea6a929a80b0fbf1b564
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-08-30 21:38:57 +0600

    Values and variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,7 @@
 let js = "amazing";
-if (js === "amazing") {
-  alert("JavaScript is FUN!");
-}
-
 console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
