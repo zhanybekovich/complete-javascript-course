3f211d0d432229484e199cf72542524942027e67
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-09-06 20:52:49 +0600

    Type conversion and coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,44 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
-console.log("Jonas");
-console.log(23);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Jonas";
-console.log(firstName);
+// let firstName = "Jonas";
+// console.log(firstName);
+
+/**
+ * Type Conversion is when we explicitly convert types
+ */
+
+// Conversion to number
+let inputYear = "1991";
+inputYear = Number(inputYear);
+console.log(inputYear + 18);
+
+let a = "Hello";
+let b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+console.log(+"5" + 5);
+console.log(parseFloat("50.9") + 5);
+console.log(parseInt("50") + 5);
+
+// conversion to string
+console.log(String(23));
+console.log("" + 45);
+
+// conversion to boolean
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+// type coercion
+console.log("I'm" + 23 + "years old");
+console.log("23" - "10" - 3);
+console.log("23" / "2");
+console.log("23" * "18");
